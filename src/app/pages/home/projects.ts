784713244811dc7144271dcf876fbdba49d415a9
Junaid Mehmood <121846768+junaidmehmood67@users.noreply.github.com>
2025-07-23 17:34:24 +0500

    Major release after refactoring

--- src/app/pages/home/projects.ts
+++ src/app/pages/home/projects.ts
@@ -2,25 +2,25 @@ export const projects = [
     {
         title: 'Mobile App Development',
         description: 'Mobile app designed to help users discover and explore local restaurants and cuisines.',
-        img: '../../../assets/image-7.svg',
+        img: '../../../assets/projects/project.jpg',
         link: '#'
     },
     {
         title: 'Landing Page Development',
         description: 'Promotional landing page for a fitness website Summer Campaign. Form development included.',
-        img: '/Next.js-Tailwind-CSS-Portfolio-Template/image/blog2.svg',
+        img: '../../../assets/projects/project.jpg',
         link: '#'
     },
     {
         title: 'Mobile App Development',
         description: 'Mobile app designed to help users discover and explore local restaurants and cuisines.',
-        img: '/Next.js-Tailwind-CSS-Portfolio-Template/image/blog3.svg',
+        img: '../../../assets/projects/project.jpg',
         link: '#'
     },
     {
         title: 'E-commerce development',
         description: 'Ecommerce website offering access to the latest and greatest gadgets and accessories.',
-        img: '/Next.js-Tailwind-CSS-Portfolio-Template/image/blog4.svg',
+        img: '../../../assets/projects/project.jpg',
         link: '#'
     }
     // Add more if needed
